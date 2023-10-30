e26ff5d9ea3a354b9328dd8a94c9b8188ca7d21e
Mahin Hasan <mahinmtrs@gmail.com>
2023-10-30 22:35:25 +0600

    added blog feed dynamically

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Blog from '../Blog/Blog';
 
 const Blogs = () => {
     const [blogs, setBlogs] = useState([])
@@ -11,6 +12,13 @@ const Blogs = () => {
     return (
         <div className='md:w-2/3'>
             <h2 className='text-4xl'>Blogs: {blogs.length}</h2>
+            {
+                blogs.map(blog => <Blog
+                    key={blog.id}
+                    blog={blog}
+                ></Blog>)
+            }
+
         </div>
     );
 };
